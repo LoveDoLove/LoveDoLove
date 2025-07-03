6c409e9ec5d6199f04964e2d21d5bf53f030b1bb
LoveDoLove <v0130p5100cuboss@gmail.com>
2025-07-03 23:37:12 +0800

    Setup portfolio

---
+++ portfolio/next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
+
+// added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
+import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare';
+initOpenNextCloudflareForDev();
